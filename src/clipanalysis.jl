6dc6244c3a145122a087ee062bfbffc0d9f79739
August Guang <august.guang@gmail.com>
2020-04-21 15:52:31 -0400

    feat(src): clipanalysis, extractreads
    Functions for clipped reads analysis and extracting reads from BAMs

---
+++ src/clipanalysis.jl
@@ -0,0 +1,84 @@
+using Gadfly
+import Cairo, Fontconfig
+
+"""
+  clippedStats(reader)
+  clippedStats(reader, chrom::String, chromrange::UnitRange)
+
+Returns the number of reads that were clipped from the BAM file, or
+from a region as readsClipped, readsTotal, and a DataFrame containing
+all of the reads mapped to the region with the read type (soft/hard/both/not)
+and mean quality of the read
+"""
+function clippedStats(reader::XAM.BAM.Reader, chrom::String, chromrange::UnitRange)
+  type = String[]
+  qual = Float64[]
+  readsClipped = 0
+  readsTotal = 0
+  for record in XAM.BAM.GenomicFeatures.eachoverlap(reader, chrom, chromrange)
+    readsTotal = readsTotal + 1
+    clippedStatsHelper!(record,qual,type,readsClipped)
+  end
+  return readsClipped, readsTotal, DataFrames.DataFrame(type=type,qual=qual)
+end
+
+function clippedStats(reader::XAM.BAM.Reader)
+  type = String[]
+  qual = Float64[]
+  readsClipped = 0
+  readsTotal = 0
+  for record in reader
+    readsTotal = readsTotal + 1
+    clippedStatsHelper!(record,qual,type,readsClipped)
+  end
+  return readsClipped, readsTotal, DataFrames.DataFrame(type=type,qual=qual)
+end
+
+function clippedStatsHelper!(record::XAM.BAM.Record, qual::Float64[],type::String[],
+  readsClipped::Int8)
+  cigar = XAM.BAM.cigar_rle(record)
+  push!(qual,Statistics.mean(XAM.BAM.quality(record)))
+  flag = "not"
+  if findfirst(x->x==convert(BioAlignments.Operation,'S'),cigar[1]) != nothing
+    flag = "soft"
+  end
+  if findfirst(x->x==convert(BioAlignments.Operation,'H'),cigar[1])!= nothing
+    if flag == "soft"
+      push!(type,"both")
+    else
+      push!(type,"hard")
+    end
+    readsClipped = readsClipped + 1
+  else
+    if flag == "soft"
+      push!(type,"soft")
+      readsClipped = readsClipped + 1
+    else
+      push!(type,"not")
+    end
+  end
+  return type,qual,readsClipped
+end
+
+"""
+  clippedReadQual(df::DataFrame, outfile::String="clippedReadQual.pdf")
+
+Plots a histogram of mean read quality split by whether the read was soft-clipped,
+hard-clipped, both, or not clipped and saves it to outfile.
+"""
+function clippedReadQual(df::DataFrames.DataFrame, outfile::String="clippedReadQual.pdf")
+  p = plot(df,x="qual", color="type", Geom.histogram,
+    Guide.title("Quality score distribution for clipped and not in region"),
+    Guide.xlabel("Mean quality"),
+    Guide.ylabel("Number of reads"))
+  p |> PDF(outfile)
+end
+
+#using BenchmarkTools
+#@benchmark
+#reader = open(BAM.Reader, "/Users/aguang/CORE/scratch/Bliss_71991_101.dedup.rg.srtd.bam",
+#  index="/Users/aguang/CORE/scratch/Bliss_71991_101.dedup.rg.srtd.bam.bai")
+#df = clippedStats("Ca22chr4A_C_albicans_SC5314",726523:727338,reader)
+#print(df)
+#clippedReadQual(df)
+#close(reader)
---
+++ src/extractReads.jl
@@ -0,0 +1,16 @@
+"""
+  extractReads(reader::XAM.BAM.Reader, chrom::String, chromrange::UnitRange)
+
+Extracts reads from a region as a FASTQ file.
+"""
+function extractReads(reader::XAM.BAM.Reader, chrom::String,
+  chromrange::UnitRange, outfile::String)
+  open(FASTX.FASTQ.Writer, outfile) do w
+    for record in XAM.BAM.GenomicFeatures.eachoverlap(reader, chrom, chromrange)
+    	rec = FASTX.FASTQ.Record(XAM.BAM.tempname(record), XAM.BAM.sequence(record),
+        XAM.BAM.quality(record))
+        write(w, rec)
+    end
+  end
+end
+#TODO: outfile as gzipped file
